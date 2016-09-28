0c91fe963fe7bcf9b3b8b27d70039bdf0d78a0f0
Ivan Borzenkov <ivan.borzenkov@gmail.com>
2016-09-28 17:38:41 +0300

    docker

--- src/Service/Storage.php
+++ src/Service/Storage.php
@@ -2,15 +2,20 @@
 
 namespace Ivan1986\DevContainer\Service;
 
+use Docker\API\Model\ContainerConfig;
+use Docker\API\Model\HostConfig;
 use Docker\Docker;
-use Docker\DockerClient;
 use Eloquent\Composer\Configuration\Element\Configuration as ComposerConfiguration;
+use Http\Client\Common\Exception\ClientErrorException;
+use Http\Client\Common\Exception\ServerErrorException;
 use Symfony\Component\Console\ConsoleEvents;
 use Symfony\Component\Console\Event\ConsoleCommandEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class Storage implements EventSubscriberInterface
 {
+    const IMAGE = 'ivan1986/dev-container';
+
     /**
      * @var ComposerConfiguration
      */
@@ -46,6 +51,12 @@ class Storage implements EventSubscriberInterface
 
     public function up()
     {
+        $container = null;
+        try {
+            $container = $this->docker->getContainerManager()->find($this->name);
+        } catch (ClientErrorException $e) {
+            $container = $this->build();
+        }
         echo 'up';
 
     }
@@ -63,6 +74,33 @@ class Storage implements EventSubscriberInterface
 
     public function rebuild()
     {
-        echo 'rebuild';
+        try {
+            $container = $this->docker->getContainerManager()->remove($this->name);
+        } catch (ClientErrorException $e) {
+        }
+        $this->build();
+    }
+
+    protected function build()
+    {
+        $config = new ContainerConfig();
+        $config
+            ->setImage(self::IMAGE)
+            ->setVolumes([
+                '/srv/web/' . $this->name => new \ArrayObject(),
+            ])
+            ->setHostConfig(
+                (new HostConfig())
+                    ->setBinds([ PROJECT_DIR . ':/srv/web/' . $this->name ])
+            )
+        ;
+        try {
+            $this->docker->getContainerManager()->create($config, ['name' => $this->name]);
+        } catch (ServerErrorException $e) {
+            echo $e->getResponse()->getStatusCode() .' '.$e->getResponse()->getBody();
+        } catch (ClientErrorException $e) {
+            echo $e->getResponse()->getStatusCode() .' '. $e->getResponse()->getBody();
+        }
+        echo 'build';
     }
 }
